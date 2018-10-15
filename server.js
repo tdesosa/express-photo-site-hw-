e393e56fb875bd4a352e2f1d5b559e0122616a5e
tdesosa <tdesosa@gmail.com>
2018-10-15 16:54:08 -0600

    app connected to mongoDb via mongoose

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
+// Require Mongo Db
+require('./db/db')
+
 
 // SERVER IS LISTNEING
 
 app.listen(port, ()=>{
     console.log(`Express Server Up & Running On ${port}`);
 });
-
-module.exports = app;
